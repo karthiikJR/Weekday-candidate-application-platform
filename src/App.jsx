cb5dfcdf4f85ee1f288d501933cd65e4537e4653
KarthikJR <karthikjrao69@gmail.com>
2024-05-04 12:35:16 +0530

    chore: Add MUI theme provider with Lexend font

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,30 @@ import './App.css'
 
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-
 import Layout from "./components/Layout";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const THEME = createTheme({
+	typography: {
+		fontFamily: `"Lexend", "Helvetica", "Arial", sans-serif`,
+		fontSize: 14,
+		fontWeightLight: 300,
+		fontWeightRegular: 400,
+		fontWeightMedium: 500,
+	},
+});
 
 function App() {
-  
-
-  return (
-		<Layout>
-			<Stack spacing={2} direction="row">
-				<Button variant="text">Text</Button>
-				<Button variant="contained">Contained</Button>
-				<Button variant="outlined">Outlined</Button>
-			</Stack>
-		</Layout>
+	return (
+		<ThemeProvider theme={THEME}>
+			<Layout>
+				<Stack spacing={2} direction="row">
+					<Button variant="text">Text</Button>
+					<Button variant="contained">Contained</Button>
+					<Button variant="outlined">Outlined</Button>
+				</Stack>
+			</Layout>
+		</ThemeProvider>
 	);
 }
 
