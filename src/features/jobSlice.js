29b8fffa792a86a01793ff833961107a8884040a
KarthikJR <karthikjrao69@gmail.com>
2024-05-05 13:10:13 +0530

    feat: Add jobSlice.js and store.js for managing job data in Redux store

---
+++ src/features/jobSlice.js
@@ -0,0 +1,81 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchJobs = createAsyncThunk(
+	"job/fetchJobs",
+	async (object, { getState, rejectWithValue }) => {
+		console.log("check check", object, getState());
+		const offset = Number(getState().offset);
+		console.log(
+			JSON.stringify({
+				limit: 10,
+				offset,
+			})
+		);
+		try {
+			const response = await fetch(
+				"https://api.weekday.technology/adhoc/getSampleJdJSON",
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						limit: 10,
+						offset: offset,
+					}),
+				}
+			);
+			const data = await response.json();
+			console.log(data.jdList);
+			return data.jdList;
+		} catch (error) {
+			return rejectWithValue(error);
+		}
+	}
+);
+
+const initialState = {
+	jobs: [],
+	filters: {
+		roles: [],
+		experience: 0,
+		companyName: "",
+		location: "",
+		remote: false,
+		minBasePay: 0,
+	},
+	offset: 1,
+	loading: false,
+	error: null,
+};
+
+const jobSlice = createSlice({
+	name: "job",
+	initialState,
+	reducers: {
+		setFilters: (state, action) => {
+			state.filters = action.payload;
+		},
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchJobs.pending, (state) => {
+				state.loading = true;
+				state.error = null;
+			})
+			.addCase(fetchJobs.fulfilled, (state, action) => {
+				state.loading = false;
+				state.error = null;
+				console.log(action);
+				state.jobs = [...state.jobs, ...action.payload];
+			})
+			.addCase(fetchJobs.rejected, (state, action) => {
+				state.loading = false;
+				state.error = action.error.message || "An error occurred";
+			});
+	},
+});
+
+export const { setFilters } = jobSlice.actions;
+
+export default jobSlice.reducer;
---
+++ src/store/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import jobReducer from "../features/jobSlice";
+
+export const store = configureStore({
+	reducer: jobReducer,
+});
