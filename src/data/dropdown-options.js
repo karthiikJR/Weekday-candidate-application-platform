3ea915564cdb528a337908f41e24e5b05da8b585
KarthikJR <karthikjrao69@gmail.com>
2024-05-05 23:04:26 +0530

    refactor: Update dropdown options for salary range

--- src/data/dropdown-options.js
+++ src/data/dropdown-options.js
@@ -1,12 +1,12 @@
 export const roles = [
-	{ data: "Frontend", type: "Engineering" },
-	{ data: "Backend", type: "Engineering" },
-	{ data: "IOS", type: "Engineering" },
-	{ data: "React Native", type: "Engineering" },
-	{ data: "Flutter", type: "Engineering" },
-	{ data: "Android", type: "Engineering" },
-	{ data: "TechLead", type: "Engineering" },
-	{ data: "Dev-Ops", type: "Engineering" },
+	{ data: "Frontend", type: "ENGINEERING" },
+	{ data: "Backend", type: "ENGINEERING" },
+	{ data: "IOS", type: "ENGINEERING" },
+	{ data: "React Native", type: "ENGINEERING" },
+	{ data: "Flutter", type: "ENGINEERING" },
+	{ data: "Android", type: "ENGINEERING" },
+	{ data: "TechLead", type: "ENGINEERING" },
+	{ data: "Dev-Ops", type: "ENGINEERING" },
 	{ data: "Designer", type: "DESIGN" },
 	{ data: "Design Manager", type: "DESIGN" },
 	{ data: "Graphic Designer", type: "DESIGN" },
@@ -16,6 +16,6 @@ export const roles = [
 
 export const experience = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-export const salary = ["0", "10", "20", "30", "40", "50", "60", "70"];
+export const salary = [0, 10, 20, 30, 40, 50, 60, 70];
 
 export const location = ["Remote", "On-site"];
